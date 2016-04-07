749655c81b92a0ed3a070b53d89bd26e3cd37977
Kentaro Doba <glass5er@gmail.com>
2016-04-07 17:42:17 +0900

    Disabled nkf. Use english column name.

--- scripts/plotM2jCurrency.py
+++ scripts/plotM2jCurrency.py
@@ -25,21 +25,20 @@ def downloadCurrencyData(fileOut, tick="d"):
 	fp.write(buff.getvalue())
 	fp.close()
 
-def toUtf8(fileIn, fileOut):
-	import nkf
-	strIn  = open(fileIn, "rt").read()
-	strOut = nkf.nkf("u", strIn)
-
-	fp = open(fileOut, "wt")
-	fp.write(strOut)
-	fp.close()
+#def toUtf8(fileIn, fileOut):
+#	import nkf
+#	strIn  = open(fileIn, "rt").read()
+#	strOut = nkf.nkf("u", strIn)
+#
+#	fp = open(fileOut, "wt")
+#	fp.write(strOut)
+#	fp.close()
 
 if __name__ == '__main__':
 	#  Choose target currency exchange in 'pairList' above.
 	target = "TRYJPY"
 	#  Choose tick in (day, week, month).
 	tick = "m"
-
 
 	fileTmpSjis = "m2j_urrency_%s_sjis.csv" % tick
 	fileTmpUtf8 = "m2j_urrency_%s_utf8.csv" % tick
@@ -48,21 +47,22 @@ if __name__ == '__main__':
 		print "Downloading currency data..."
 		downloadCurrencyData(fileTmpSjis, tick=tick)
 
-	if not os.path.exists(fileTmpUtf8):
-		print "Converting character code..."
-		toUtf8(fileTmpSjis, fileTmpUtf8)
-
+	#if not os.path.exists(fileTmpUtf8):
+	#	print "Converting character code..."
+	#	toUtf8(fileTmpSjis, fileTmpUtf8)
 
 	colIdx = 6 * pairDict[target]
-	df = pd.read_csv(fileTmpUtf8, skiprows=1, usecols=[0] + list(range(colIdx+1, colIdx+5)), parse_dates=[0])
+	df = pd.read_csv(fileTmpSjis, skiprows=1, usecols=[0] + list(range(colIdx+1, colIdx+5)), parse_dates=[0])
+	#  Rename colmuns to English.
+	df.columns = ["Date", "Open", "High", "Low", "Close"]
 	#  @TODO:  Define datetime range. Use last 1000 values for now.
 	df = df[-100:]
 
-	dates = df["日付"]
-	df["日付"] = dates.values.astype("datetime64[D]").astype(float)
+	dates = df["Date"]
+	df["Date"] = dates.values.astype("datetime64[D]").astype(float)
 
 	step = df.shape[0] / 10
-	plt.xticks(df["日付"][::step], [x.strftime("%Y/%m/%d") for x in dates[::step]], rotation=20)
+	plt.xticks(df["Date"][::step], [x.strftime("%Y/%m/%d") for x in dates[::step]], rotation=20)
 
 	ax = plt.subplot()
 	candlestick_ohlc(ax, df.values)
